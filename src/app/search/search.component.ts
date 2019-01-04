ec26f1ca8f3f1351abab3f0574497960c184f295
jacktown11 <jikai_tang@163.com>
2019-01-04 22:26:50 +0800

    finish chapter 7

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  formModel: FormGroup;
+  categories: string[];
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder, private productService: ProductService) {
+    this.formModel = fb.group({
+      productTitle: ['', [Validators.required, Validators.minLength(3)]],
+      productPrice: [null, positiveNumberValidator],
+      category: [-1]
+    });
   }
 
+  ngOnInit() {
+    this.categories = this.productService.getCategories();
+  }
+
+  submitForm() {
+    console.log(this.formModel.status);
+    if (this.formModel.valid) {
+      console.log(this.formModel.value);
+    } else {
+      console.log('表单数据不完全正确，不能提交');
+    }
+  }
+}
+export function positiveNumberValidator(numberCtr: AbstractControl): any {
+  let value = numberCtr.value;
+  if (value == null) return null;
+  let num = parseInt(value);
+  return num <= 0 ? { positive: { info: '必须是正数' } } : null;
 }
--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -6,9 +6,9 @@ import { Injectable } from '@angular/core';
 export class ProductService {
   private products: Product[] = [
     new Product(1, '第一个商品', 1.99, 3.5, '这是一个商品描述。。。。。。。', ['电子产品', '硬件设备']),
-    new Product(2, '第二个商品', 1.99, 4.5, '这是一个商品描述。。。。。。。', ['电子产品', '硬件设备']),
+    new Product(2, '第二个商品', 1.99, 4.5, '这是一个商品描述。。。。。。。', ['电子产品', '图书']),
     new Product(3, '第三个商品', 1.99, 1, '这是一个商品描述。。。。。。。', ['硬件设备']),
-    new Product(4, '第四个商品', 1.99, 5, '这是一个商品描述。。。。。。。', ['电子产品']),
+    new Product(4, '第四个商品', 1.99, 5, '这是一个商品描述。。。。。。。', ['电子产品', '图书']),
     new Product(5, '第五个商品', 1.99, 4.5, '这是一个商品描述。。。。。。。', ['硬件设备']),
     new Product(6, '第六个商品', 1.99, 2.5, '这是一个商品描述。。。。。。。', ['电子产品', '硬件设备']),
   ];
@@ -23,6 +23,16 @@ export class ProductService {
 
   constructor() { }
 
+  getCategories(): string[] {
+    return this.products.reduce((prev, cur) => {
+      for (let item of cur.categories) {
+        if (prev.indexOf(item) < 0) {
+          prev.push(item);
+        }
+      }
+      return prev;
+    }, []);
+  }
   getAllProducts(): Product[] {
     return this.products;
   }
